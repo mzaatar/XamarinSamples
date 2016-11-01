2c02a064b61c5ae64490f1dac4eed69c922fd80b
mzaatar <mohamed.zaatar@gmail.com>
2016-11-01 09:36:02 +0800

    Initial commit

---
+++ PhonewordPortalable/PhonewordPortalable/PhonewordPortalable/CallHistoryPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace PhonewordPortalable
+{
+    public partial class CallHistoryPage : ContentPage
+    {
+        public CallHistoryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnClear(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                     "Clear history",
+                     "Delete all call history?",
+                     "Yes",
+                     "No"))
+            {
+                App.PhoneNumbers.Clear();
+                await Navigation.PopAsync();
+            }
+        }
+    }
+}
---
+++ PhonewordPortalable/PhonewordPortalable/PhonewordPortalable/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace PhonewordPortalable
+{
+    public partial class MainPage : ContentPage
+    {
+        string translatedNumber;
+        string originalNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTranslate(object sender, EventArgs e)
+        {
+            originalNumber = phoneNumberText.Text;
+            translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+            if (!string.IsNullOrWhiteSpace(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        async void OnCall(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                    "Dial a Number",
+                    "Call " + translatedNumber + "?",
+                    "Yes",
+                    "No"))
+            {
+                var dialer = DependencyService.Get<IDialer>();
+                if (dialer != null)
+                {
+                    if (App.PhoneNumbers.ContainsKey(originalNumber))
+                    {
+                        App.PhoneNumbers.Remove(originalNumber);
+                    }
+                    App.PhoneNumbers.Add(originalNumber,translatedNumber);
+                    dialer.Dial(translatedNumber);
+                }
+            }
+        }
+
+        async void OnCallHistory(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CallHistoryPage());
+        }
+    }
+}
